6d46845f612aaa1a5a17536990488f0ce3b300a0
BADI-NATI <mentalet@gmail.com>
2023-08-10 23:58:05 -0700

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function.
+ *
+ * Return:  must always be zero.
+ */
+
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of char: %d byte(s)\n", sizeof(int));
+	printf("Size of char: %d byte(s)\n", sizeof(long));
+	printf("Size of char: %d byte(s)\n", sizeof(long long));
+	printf("Size of char: %d byte(s)\n", sizeof(float));
+	return (0);
+}
