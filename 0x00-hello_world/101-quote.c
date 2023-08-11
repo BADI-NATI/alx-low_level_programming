b35c73b160405b377f689598640c20d0151c268c
BADI-NATI <mentalet@gmail.com>
2023-08-11 01:07:39 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - this is the main function.
  *
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	char message[] = "with proper grammar, but the outcome is a piece of art,\n";
-        write(1, message, sizeof(message) - 1);
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(1, message, sizeof(message) - 1);
 	return (0);
 }
