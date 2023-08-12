8b629cb0445a5ac05b5ad40a77e7a2f387335519
BADI-NATI <mentalet@gmail.com>
2023-08-11 19:08:33 -0700

    sign

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,9 +18,9 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive", n);
-	else if (n < 0)
-		printf("%d is negative", n);
-	else 
+	else if (n == 0)
 		printf("%d is zero", n);
+	else 
+		printf("%d is negative", n);
 	return (0);
 }
