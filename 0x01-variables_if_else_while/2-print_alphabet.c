8d1111b8ba35014edb9350c5aed155fcf3573a5f
BADI-NATI <mentalet@gmail.com>
2023-08-13 02:50:10 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - this is the main function.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 65; n < 91; n++)
+		putchar(n);
+	printf("\n");
+	return (0);
+}
