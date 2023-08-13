a5cc746b79e349819d98dac18bc6977e70c975ab
BADI-NATI <mentalet@gmail.com>
2023-08-13 04:35:21 -0700

    Hexadcimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - this is the main function.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (i = 97; i <= 102; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
