3dcd15d7b2094405472437134cd97ded9b0f137e
BADI-NATI <mentalet@gmail.com>
2023-08-13 06:28:35 -0700

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - this is the main function.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			putchar(i);
+			putchar(n);
+			if (i == 56 && n == 57)
+				continue;
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
