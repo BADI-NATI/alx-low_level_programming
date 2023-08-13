e07549172b134eea81657227e776b68657100573
BADI-NATI <mentalet@gmail.com>
2023-08-13 04:49:41 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - this is the main function.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+			continue;
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
