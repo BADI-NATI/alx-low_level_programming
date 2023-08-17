6c8819afc14078cd5a9b826876307ecff1e9a49c
BADI-NATI <mentalet@gmail.com>
2023-08-17 08:00:13 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,27 +7,35 @@
 
 void times_table(void)
 {
-	int i, j, product;
+    int i, j, product;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			product = i * j;
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            product = i * j;
 
-			if (product < 10)
-				_putchar(' ');
-			else
-				_putchar((product / 10) + '0');
-			_putchar((product % 10) + '0');
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
 
-			if (j == 9)
-				_putchar('\n');
-		}
-	}
+            if (product < 10)
+            {
+                if (j != 0)
+                    _putchar(' ');
+
+                _putchar('0' + product);
+            }
+            else
+            {
+                _putchar((product / 10) + '0');
+                _putchar((product % 10) + '0');
+            }
+
+            if (j == 9)
+                _putchar('\n');
+        }
+    }
 }
